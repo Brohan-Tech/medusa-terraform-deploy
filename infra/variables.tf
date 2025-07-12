f166c3eae88805870b3ed347e8a8f15ae3941da4
Rohana Upadhyay <workrohanu23@gmail.com>
2025-07-12 22:54:00 +0000

    Removed aws_access_key_id and secret from variables.tf

--- infra/variables.tf
+++ infra/variables.tf
@@ -14,15 +14,3 @@ variable "aws_region" {
   type        = string
   default     = "us-east-1"
 }
-variable "aws_access_key_id" {
-  description = "AWS Access Key ID"
-  type        = string
-  sensitive   = true
-}
-
-variable "aws_secret_access_key" {
-  description = "AWS Secret Access Key"
-  type        = string
-  sensitive   = true
-}
-
